e01a2ea9ef4760ea66735d060f0d20f9b9eb8553
blackfrieza2002 <blackfrieza2002@gmail.com>
2026-07-28 07:36:07 +0600

    Create nvim.sh

---
+++ nvim.sh
@@ -0,0 +1,50 @@
+apt install -y neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+touch ~/.config/nvim/init.vim
+nvim ~/.config/nvim/init.vim
+code:
+
+call plug#begin()
+" Use release branch (recommended)
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+
+call plug#end()
+""" coc config
+" ---- CoC Automatically Managed Extensions Marketplace List ----
+let g:coc_global_extensions = [
+  \ 'coc-html',
+  \ 'coc-css',
+  \ 'coc-tsserver',
+  \ 'coc-pyright',
+  \ 'coc-clangd',
+  \ 'coc-sh',
+  \ 'coc-prettier'
+  \ ]
+  
+inoremap <silent><expr> <TAB>
+      \ coc#pum#visible() ? coc#pum#next(1) :
+      \ CheckBackspace() ? "\<Tab>" :
+      \ coc#refresh()
+inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+" Make <CR> to accept selected completion item or notify coc.nvim to format
+" <C-g>u breaks current undo, please make your own choice
+inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+
+""" Main Configurations
+filetype plugin indent on
+set tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab autoindent
+set incsearch ignorecase smartcase hlsearch
+set wildmode=longest,list,full wildmenu
+set ruler laststatus=2 showcmd showmode
+set list listchars=trail:»,tab:»-
+set fillchars+=vert:\ 
+set wrap breakindent
+set encoding=utf-8
+set textwidth=0
+set hidden
+set number
+set title
