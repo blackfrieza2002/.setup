be99b9fa00b92e8f8572b84eda510281436fb604
blackfrieza2002 <blackfrieza2002@gmail.com>
2026-07-28 09:09:11 +0600

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,12 +1,13 @@
 apt update -y
-apt upgrade -y
 apt install -y x11-repo
 apt install -y tur-repo
 apt install -y tigervnc
 apt install -y fluxbox
 apt install -y firefox
 apt install -y code-oss
+apt install -y code-is-code-oss
 apt install -y code-server
+apt install -y wireshark-qt
 
 vncserver
 vncserver -kill :1
@@ -14,3 +15,5 @@ echo "fluxbox &" >> ~/.vnc/xstartup
 echo "vncserver -geometry 1650x720" >> ~/vnc
 mv vnc /data/data/com.termux/files/usr/bin/
 chmod +x /data/data/com.termux/files/usr/bin/vnc
+
+code-server & sleep 3 && nano ~/.config/code-server/config.yaml
